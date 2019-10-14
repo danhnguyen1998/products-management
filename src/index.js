2106a364cb43f351247ab96822f2e277f042b69e
Mr. Toxic <mrtoxic@Mr-Toxic.local>
2019-10-15 00:13:14 +0700

    fix issue display on chrome

--- src/index.js
+++ src/index.js
@@ -9,12 +9,7 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    myReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
-        composeEnhancer(applyMiddleware(thunk)) 
-);
+const store = createStore(myReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
